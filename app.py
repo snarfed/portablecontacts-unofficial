a7d0ca720bee887f8737c309f32ec4f0568181ad
Ryan Barrett <git@ryanb.org>
2013-10-16 18:27:24 -0700

    port to new oauth-dropins API
    consolidates facebook_demo.py and twitter_demo.py into app.py.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Serves the HTML front page and discovery files.
+"""Serves the the front page, discovery files, and OAuth flows.
 
 The discovery files inside /.well-known/ include host-meta (XRD), and
 host-meta.xrds (XRDS-Simple), and host-meta.jrd (JRD ie JSON).
@@ -14,11 +14,19 @@ http://hueniverse.com/drafts/draft-xrds-simple-01.html
 
 __author__ = 'Ryan Barrett <portablecontacts@ryanb.org>'
 
+import urllib
+
 import appengine_config
 import poco
+import webapp2
 from webutil import handlers
 
-import webapp2
+from oauth_dropins import facebook
+from oauth_dropins import twitter
+site_module = {'facebook-poco': facebook,
+               'twitter-poco': twitter,
+               }[appengine_config.APP_ID]
+
 
 class FrontPageHandler(handlers.TemplateHandler):
   """Renders and serves /, ie the front page.
@@ -31,7 +39,19 @@ class FrontPageHandler(handlers.TemplateHandler):
             'auth_url': poco.SOURCE.AUTH_URL,
             }
 
+class CallbackHandler(site_module.CallbackHandler):
+  def finish(self, auth_entity, state=None):
+    return_params = {'facebook-poco': ('access_token',),
+                     'twitter-poco': ('token_key', 'token_secret'),
+                     }[appengine_config.APP_ID]
 
-application = webapp2.WSGIApplication(
-  [('/', FrontPageHandler)] + handlers.HOST_META_ROUTES,
+    self.redirect('/?%s' % urllib.urlencode(
+        {k: getattr(auth_entity, k) for k in return_params}))
+
+
+application = webapp2.WSGIApplication([
+    ('/', FrontPageHandler),
+    ('/start_auth', site_module.StartHandler.to('/oauth_callback')),
+    ('/oauth_callback', CallbackHandler),
+    ] + handlers.HOST_META_ROUTES,
   debug=appengine_config.DEBUG)
